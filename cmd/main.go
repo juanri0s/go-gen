5588a6efa9e58ebb8c0e96e92348e4cc5204d722
Juan Rios <juan.rios.28@gmail.com>
2020-08-06 13:24:18 -0400

    add: simple graceful shutdown for server

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -10,28 +14,39 @@ const version = "1.0.0"
 const timeout = 120 * time.Second
 
 func main() {
-	s := &http.Server{
+	srv := &http.Server{
 		Addr:         ":8080",
 		ReadTimeout:  timeout,
 		WriteTimeout: timeout,
 	}
 
-	// TODO: graceful shutdown
 	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/repository", repoHandler)
 
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+		sig := <-quit
+		fmt.Println("received shutdown signal:", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			fmt.Println("http server: %s", err.Error())
+		}
+	}()
+
 	fmt.Printf("Starting server at port 8080\n")
-	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		fmt.Println("http server:%s", err.Error())
 		return
 	}
-
-	// TODO: listen for signals
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`healthy`))
+	w.Write([]byte(`HEALTHY ` + version))
 }
 
 func repoHandler(w http.ResponseWriter, r *http.Request) {
