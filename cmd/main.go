7a1118048549798b0117d5ee197cfab5d0ceabdd
Juan Rios <juan.rios.28@gmail.com>
2020-08-06 11:09:03 -0400

    add: initial server using standard lib

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+var version = "1.0.0"
 
 func main() {
-	fmt.Println("Hello")
+	// TODO: sample, remove
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello")
+	})
+
+	http.ListenAndServe(":8080", nil)
+	// TODO: health check and graceful shutdown
 }
