c10e65680ec3955ea9d06fb53188eaebe8810173
Juan Rios <juan.rios.28@gmail.com>
2020-08-06 12:55:41 -0400

    add: repo endpoint for creation of new repos

--- cmd/main.go
+++ cmd/main.go
@@ -18,6 +18,7 @@ func main() {
 
 	// TODO: graceful shutdown
 	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/repository", repoHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -31,4 +32,14 @@ func main() {
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`healthy`))
+}
+
+func repoHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`created`))
+	default:
+		fmt.Fprintf(w, "Only GET and POST methods are supported")
+	}
 }
