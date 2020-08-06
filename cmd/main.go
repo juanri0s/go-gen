a0cfd0894ec853aa316f155d64ea2e99d64e3f5f
Juan Rios <juan.rios.28@gmail.com>
2020-08-06 12:49:07 -0400

    add: simple server

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-var version = "1.0.0"
+const version = "1.0.0"
+const timeout = 120 * time.Second
 
 func main() {
-	// TODO: sample, remove
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello")
-	})
+	s := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  timeout,
+		WriteTimeout: timeout,
+	}
 
-	http.ListenAndServe(":8080", nil)
-	// TODO: health check and graceful shutdown
+	// TODO: graceful shutdown
+	http.HandleFunc("/health", healthHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		fmt.Println("http server:%s", err.Error())
+		return
+	}
+
+	// TODO: listen for signals
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`healthy`))
 }
