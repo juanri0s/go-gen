edd18c9b247b33f0875b714967e53adb38ce2f8d
Juan Rios <juan.rios.28@gmail.com>
2020-08-09 02:13:27 -0400

    add: generate from spec file where we will run the cli

--- cmd/commands/commands.go
+++ cmd/commands/commands.go
@@ -1,1 +1,16 @@
 package commands
+
+import (
+	"flag"
+	"fmt"
+)
+
+func generateFromSpec() {
+	fPackage := flag.String("package", "main", "package test")
+	fCopyright := flag.Bool("copyright", true, "copyright test")
+	fImports := flag.String("imports", "", "import test")
+
+	flag.Parse()
+
+	fmt.Printf("textPtr: %s, metricPtr: %t, uniquePtr: %s\n", *fPackage, *fCopyright, *fImports)
+}
