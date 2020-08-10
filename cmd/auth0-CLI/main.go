b9c5f0da632dc5b9232c1b5b42a397df4c3b1423
Juan Rios <juan.rios.28@gmail.com>
2020-08-10 18:18:21 -0400

    removed cli start log

--- cmd/auth0-CLI/main.go
+++ cmd/auth0-CLI/main.go
@@ -4,11 +4,9 @@ import (
 	"os"
 
 	"github.com/juanri0s/auth0-exercise/internal/app"
-	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	log.Info("auth0-cli started")
 	err := app.StartCLI(os.Args)
 	if err != nil {
 		os.Exit(1)
