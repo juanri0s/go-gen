9f600e956d5dbb2284dcadc7323752b63a758f12
Juan Rios <juan.rios.28@gmail.com>
2020-08-10 16:37:27 -0400

    add log to show cli started

--- cmd/auth0-CLI/main.go
+++ cmd/auth0-CLI/main.go
@@ -4,9 +4,11 @@ import (
 	"os"
 
 	"github.com/juanri0s/auth0-exercise/internal/app"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	log.Info("auth0-cli started")
 	err := app.StartCLI(os.Args)
 	if err != nil {
 		os.Exit(1)
